1acad6b6e712d0854defc732eab9be7af88e16e0
Porco022 <111072200+Porco022@users.noreply.github.com>
2022-11-17 19:09:29 -0300

    Add files via upload
    fatorial de 1-10

---
+++ fatorial.java
@@ -0,0 +1,20 @@
+package testarcondicional;
+
+public class fatorial {
+	public static void main(String args[]) {
+		
+		int x = 1;
+		
+		for(int n = 1; n<11; n++) {
+			x*=n;
+			
+			
+			 System.out.println("Fatorial de " + n + " = " + x);
+		}
+		
+		
+		
+		
+	}
+
+}
